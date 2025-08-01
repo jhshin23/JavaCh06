e7e52402e32ebfd819c479d94783a5089e719586
AA <180617768+jhshin23@users.noreply.github.com>
2025-08-01 19:51:06 +0900

    Page393, 이론 문제 12번

---
+++ src/ch06/AppointmentDate.java
@@ -0,0 +1,17 @@
+package ch06;
+
+import java.util.Calendar;
+//Page393, 이론 문제 12번
+public class AppointmentDate {
+
+	public static void main(String[] args) {
+		Calendar date = Calendar.getInstance();
+		date.clear();
+		date.set(Calendar.YEAR, 2020);
+		date.set(Calendar.MONTH, 11);
+		date.set(Calendar.DAY_OF_MONTH, 25);
+		
+		System.out.println(date.get(Calendar.YEAR)+ "년 " + (date.get(Calendar.MONTH)+1) + "월 " + date.get(Calendar.DAY_OF_MONTH) + "일");
+	}
+
+}
