2efdb85f657194495be9be22a2f08461537ba77d
AA <180617768+jhshin23@users.noreply.github.com>
2025-08-01 19:06:48 +0900

    Page391, P06 이론문제

---
+++ src/ch06/WrapperCodeQuiz.java
@@ -0,0 +1,23 @@
+package ch06;
+//Page391, P06 이론문제
+public class WrapperCodeQuiz {
+
+	public static void main(String[] args) {
+		//1
+		String s1 = Integer.toString(20);
+		//2
+		double d = Double.parseDouble("35.9");
+		//3
+		boolean b = Boolean.parseBoolean("true");
+		//4
+		String s2 = Integer.toBinaryString(30);
+		//5
+		String c = Character.toString('c');
+		
+		//답 확인
+		System.out.println(s1.concat(" is String."));
+		System.out.println(d+100);
+		if(b) System.out.println(s2 + c);
+	}
+
+}
