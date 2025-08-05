f22fd5f961c5d5b8efecf02bab1d8f3b67f34c87
AA <180617768+jhshin23@users.noreply.github.com>
2025-08-05 11:49:09 +0900

    Page394, P03, package import

---
+++ src/main/MainApp.java
@@ -0,0 +1,13 @@
+package main;
+
+import util.Calc;
+
+//Page394, P03
+public class MainApp {
+
+	public static void main(String[] args) {
+		Calc c = new Calc(10, 20);
+		System.out.println(c.sum());
+	}
+
+}
---
+++ src/util/Calc.java
@@ -0,0 +1,10 @@
+package util;
+//Page394, P03
+public class Calc {
+	private int x, y;
+	public Calc(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public int sum() { return x + y; }
+}
