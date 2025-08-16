c3d96ac06a8b5d354e0e24338fa2da26e0943ac5
AA <180617768+jhshin23@users.noreply.github.com>
2025-08-16 18:48:14 +0900

    Page398, P10-(1)(2), 섞인 스펠링 퀴즈, 경과 시간 기록

--- src/ch06/WordMixQuiz.java
+++ src/ch06/WordMixQuiz.java
@@ -1,8 +1,9 @@
 package ch06;
 import java.util.Scanner;
-
+//Page398, P10-(1)(2), 섞인 스펠링 퀴즈, 경과 시간 기록
 class Quiz {
-	final int LIMIT_SEC = 10;;
+	final int LIMIT_SEC = 10;
+	final int LEVEL = 10;
 	private String word[] = {
 		"lovely",
 		"sunny",
@@ -18,22 +19,24 @@ class Quiz {
 	}
 	
 	public boolean[] start(String[] mixedWord, Scanner scanner) {
-		boolean[] corrOrFail = new boolean [mixedWord.length];
-		for(int i = 0 ; i < corrOrFail.length ; i++) corrOrFail[i] = false;
+		boolean[] isCorrect = new boolean [mixedWord.length];
+		for(int i = 0 ; i < isCorrect.length ; i++) isCorrect[i] = false;
 		
 		
 		for(int i = 0 ; i < mixedWord.length ; i++) {
-			System.out.println(mixedWord[i]+ "");
+			System.out.println(mixedWord[i]);
 			System.out.print(">>");
 			
-//			int startTimeMillis = (int) System.currentTimeMillis();
+			double startTimeMillis = (double) System.currentTimeMillis();
 			String input = scanner.nextLine().trim();
-	//		int passedTimeSeconds = ((int) System.currentTimeMillis() - startTimeMillis) / 100;
-			
-			if(input.equals("그만")) return corrOrFail;
+			double passedTimeSeconds = ((double) System.currentTimeMillis() - startTimeMillis) / 1000;
+			if(input.equals("그만")) return isCorrect;
 			else if (input.equals(word[i])) {
-				corrOrFail[i] = true;
-				System.out.println("성공!!!");
+				if(passedTimeSeconds <= LIMIT_SEC) {
+					isCorrect[i] = true;
+					System.out.println("성공!!! " + passedTimeSeconds + "초 경과");
+				}
+				else System.out.println("실패!!! " + LIMIT_SEC + "초 초과. " + passedTimeSeconds + "초 경과");
 			}
 			else {
 				System.out.println("실패!!! " + word[i] + " 입니다.");
@@ -42,38 +45,42 @@ class Quiz {
 		}
 			
 		
-		return corrOrFail;
+		return isCorrect;
 		
 	}
 	
-	public String[] MixSpelling() {
+	public String[] mixSpelling() {
 		String mixedWord[] = new String[word.length];
-		for (int i = 0 ; i < word.length ; i++) {
-			char[] alphabetFromWord = word[i].toCharArray();
-			
-			int replaceCharIndex1 = (int) (Math.random()*alphabetFromWord.length);
-			int replaceCharIndex2 = (int) (Math.random()*alphabetFromWord.length);
-			while(true) {
-				if(replaceCharIndex1 == replaceCharIndex2) {
-					replaceCharIndex2 = (int) (Math.random()*alphabetFromWord.length);
-					continue;
+		for (int i = 0 ; i < word.length ; i++) mixedWord[i] = word[i];
+		
+		for(int i = 0 ; i < LEVEL ; i++) {
+			for (int j = 0 ; j < mixedWord.length ; j++) {
+				char[] alphabetFromWord = mixedWord[j].toCharArray();
+				
+				int replaceCharIndex1 = (int) (Math.random()*alphabetFromWord.length);
+				int replaceCharIndex2 = (int) (Math.random()*alphabetFromWord.length);
+				while(true) {
+					if(replaceCharIndex1 == replaceCharIndex2) {
+						replaceCharIndex2 = (int) (Math.random()*alphabetFromWord.length);
+						continue;
+					}
+					break;
 				}
-				break;
+				
+				char t = alphabetFromWord[replaceCharIndex1];
+				alphabetFromWord[replaceCharIndex1] = alphabetFromWord[replaceCharIndex2];
+				alphabetFromWord[replaceCharIndex2] = t;
+				
+				mixedWord[j] = String.valueOf(alphabetFromWord);
 			}
-			
-			char t = alphabetFromWord[replaceCharIndex1];
-			alphabetFromWord[replaceCharIndex1] = alphabetFromWord[replaceCharIndex2];
-			alphabetFromWord[replaceCharIndex2] = t;
-			
-			mixedWord[i] = String.valueOf(alphabetFromWord);
 		}
 		return mixedWord;
 	}
 	
-	public void printScore(boolean[] corrOrFail) {
+	public void printScore(boolean[] isCorrect) {
 		int cnt = 0;
-		for (boolean b : corrOrFail) if(b) cnt++;
-		System.out.println(corrOrFail.length + "개 중 "+ cnt + "개 정답");
+		for (boolean b : isCorrect) if(b) cnt++;
+		System.out.println(isCorrect.length + "개 중 "+ cnt + "개 정답");
 	}
 	
 }
@@ -83,7 +90,7 @@ public class WordMixQuiz {
 		Scanner scanner = new Scanner(System.in);
 		Quiz q = new Quiz();
 		
-		q.printScore(q.start(q.MixSpelling(), scanner));
+		q.printScore(q.start(q.mixSpelling(), scanner));
 		
 		scanner.close();
 	}
